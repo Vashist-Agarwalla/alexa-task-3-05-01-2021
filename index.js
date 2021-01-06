822e0b43369d74c648dbd255c41ad18cd05593a5
Vashist-Agarwalla <62013535+Vashist-Agarwalla@users.noreply.github.com>
2021-01-06 11:59:17 +0530

    06-01-2021

--- index.js
+++ index.js
@@ -1,40 +1,49 @@
 const fs = require("fs");
 const { exit } = require("process");
 const readline = require("readline");
-
-console.log("1. Add Address");
-console.log("2. Delete Address");
-console.log("3. View Address");
+const { start } = require("repl");
 
 const r1 = readline.createInterface({
     input: process.stdin,
     output: process.stdout
 });
 
-r1.question("Enter your choice: ", (Option) => {
-    switch (parseInt(Option)) {
-        case 1:
-            addad();
-            break;
-        case 2:
-            delad();
-            break;
-        case 3:
-            viewad();
-            break;
-        default:
-            console.log("Wrong Option!")
-            exit(0);
-    }
-})
+startprg();
+
+function startprg() {
+    console.log("\n\n1. Add Address");
+    console.log("2. Delete Address");
+    console.log("3. View Address");
+    console.log("4. Exit");
+
+    r1.question("Enter your choice: ", (Option) => {
+        switch (parseInt(Option)) {
+            case 1:
+                addad();
+                break;
+            case 2:
+                delad();
+                break;
+            case 3:
+                viewad();
+                break;
+            case 4:
+                console.log("Bye");
+                exit(0);
+            default:
+                console.log("Wrong Option!")
+                startprg();
+        }
+    })
+}
 
 var obj = {
     name: "NULL",
     address: "NULL"
 };
 
-function addad(){
-    console.log("Adding");
+function addad() {
+    // console.log("Adding");
     let dataBuffer = fs.readFileSync("data.json");
     let data = JSON.parse(dataBuffer);
     r1.question("Enter the Address: ", (Add) => {
@@ -42,35 +51,34 @@ function addad(){
             obj.name = Name;
             obj.address = Add;
             data.push(obj);
-            console.log(data.length)
             let jsonString = JSON.stringify(data);
             fs.writeFileSync("data.json", jsonString);
-            // exit(0);
+            startprg();
         });
     });
 
 }
 
-function delad(){
-    console.log("DELETING");
+function delad() {
+    // console.log("DELETING");
     let dataBuffer = fs.readFileSync("data.json");
     let data = JSON.parse(dataBuffer);
     let final = [];
     r1.question("Enter the name whose address would you like to delete: ", (Name) => {
-        for(let i=0 ; i<data.length ; i++) {
-            if(data[i].name !== Name){
+        for (let i = 0; i < data.length; i++) {
+            if (data[i].name !== Name) {
                 final.push(data[i]);
             }
         }
         let jsonString = JSON.stringify(final);
         fs.writeFileSync("data.json", jsonString);
-        exit(0);
+        startprg();
     })
 }
 
-function viewad(){
-    console.log("View")
+function viewad() {
+    // console.log("View");
     let dataBuffer = fs.readFileSync("data.json");
     console.table(JSON.parse(dataBuffer.toString()))
-    exit(0);
+    startprg();
 }
